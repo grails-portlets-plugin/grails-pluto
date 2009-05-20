ccdd20496f13c08e369af6e9ad9942f4d8e3102e
k2n <kenjin@clazzsoft.com>
2009-05-20 06:33:22 +0000

    get rid of the dependency on portlets-pluto due to GRAILS-4586.
    git-svn-id: https://svn.codehaus.org/grails-plugins/grails-pluto/trunk@52258 832c1c66-4827-0410-8465-ccb17913cd09

--- PlutoGrailsPlugin.groovy
+++ PlutoGrailsPlugin.groovy
@@ -18,7 +18,10 @@ class PlutoGrailsPlugin {
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "1.1 > *"
     // the other plugins this plugin depends on
-    def dependsOn = ['portlets-pluto':"0.1 > *"]
+    // There is a bug to prevent a plugin with "-" from resolved in grails. 
+    // see http://jira.codehaus.org/browse/GRAILS-4586
+    //def dependsOn = ["portlets-pluto":"0.1 > * ", "portlets":"0.2 > * "]
+    def dependsOn = ["portlets":"0.2 > * "]
     // resources that are excluded from plugin packaging
     def pluginExcludes = [
             "grails-app/views/error.gsp"
